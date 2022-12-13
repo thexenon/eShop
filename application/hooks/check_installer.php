48fbf1a26afb213ba2469c2d68d4b6b168ad881d
Derek Donkor <donkorderek@gmail.com>
2022-12-13 16:42:22 +0000

    Update check_installer.php

--- application/hooks/check_installer.php
+++ application/hooks/check_installer.php
@@ -2,8 +2,8 @@
 
 class Check_installer {
     function check_for_installer() {
-		if(file_exists('../install/index.php') ){
-			header("location:../install/");
+		if(file_exists('../../install/index.php') ){
+			header("location:../../install/");
 			die();
 		}
     }
